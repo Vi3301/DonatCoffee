c8cf08bbe371365bda6eff4228546d27ee4a5cbe
Vi3301 <cryptornix@gmail.com>
2023-07-07 11:49:56 +0300

    add form

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.form');
+    const form = document.querySelector('.popup_container');
     const popup = document.getElementById('popup');
     const closeButton = document.getElementById('btn-close')
-    const entranceButton = document.querySelector('.entrance-link')
+    const entranceButton = document.querySelector('.header_link_entrance')
     entranceButton.addEventListener('click', function() {
         console.log("shelk")
-        form.style.display = 'block';
+        form.style.display = 'flex';
     })
     form.addEventListener('submit', function(event) {
         event.preventDefault;
